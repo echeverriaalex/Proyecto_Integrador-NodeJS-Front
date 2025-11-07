c0cb6942ddcd38e6469a49ee69028430cb78b658
Alex Echeverria <aleex.naahuel@outlook.com>
2025-11-07 14:58:02 -0300

    asigno rutas de imagenes para genres

---
+++ src/utils/setWallpaperGenres.js
@@ -0,0 +1,45 @@
+import Action from "../assets/imageGenres/action.jpg";
+import Mystery from "../assets/imageGenres/mystery.png";
+import War from "../assets/imageGenres/war.webp";
+import Fantasy from "../assets/imageGenres/fantasy.jpg";
+import Thriller from "../assets/imageGenres/thriller.jpg";
+import History from "../assets/imageGenres/history.jpeg";
+import Occidental from "../assets/imageGenres/occidental.jpeg";
+import Horror from "../assets/imageGenres/horror.jpeg";
+import Crime from "../assets/imageGenres/Crime.jpg";
+import Documentary from "../assets/imageGenres/Documentary.jpg";
+import Western from "../assets/imageGenres/Western.webp";
+import Drama from "../assets/imageGenres/Drama.jpg";
+import Family from "../assets/imageGenres/Family.jpg";
+import Comedy from "../assets/imageGenres/Comedy.jpg";
+import Animation from "../assets/imageGenres/Animation.jpg";
+import Adventure from "../assets/imageGenres/Adventure.jpg";
+import Music from "../assets/imageGenres/Music.jpg";
+import Romance from "../assets/imageGenres/Romance.jpg";
+import ScienceFiction from "../assets/imageGenres/ScienceFiction.jpg";
+import TVMovie from "../assets/imageGenres/TVMovie.jpeg";
+
+const WallpaperGenres = {
+    Action,
+    Horror,
+    War,
+    Fantasy,
+    Thriller,
+    History,
+    Occidental,
+    Mystery,
+    Comedy,
+    Crime,
+    Documentary,
+    Western,
+    Drama,
+    Family,
+    Animation,
+    Adventure,
+    Music,
+    Romance,
+    "Science Fiction": ScienceFiction,
+    "TV Movie": TVMovie,
+};
+
+export default WallpaperGenres;

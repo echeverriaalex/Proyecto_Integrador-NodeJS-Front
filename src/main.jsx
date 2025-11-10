9b863cd9a9d6ccad709e0836c12dc45fdb33756e
Alex Echeverria <aleex.naahuel@outlook.com>
2025-11-10 14:25:03 -0300

    creo proyecto con vite e instalo dependencias

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
+import { GlobalStyles } from './components/styles/GlobalStyles.js'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 )

b5380c78a48f4f6aba029a9c1705337f9b3075db
Alex Echeverria <aleex.naahuel@outlook.com>
2025-11-10 22:45:56 -0300

    constantes necesarias

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+//export const BASE_URL2 = "http://www.omdbapi.com/";
+
+export const API_URL_ALL = "https://api.tvmaze.com/shows";
+export const API_URL_MOVIES_PAGES = "https://api.themoviedb.org/3/discover/movie";
+export const API_URL_MOVIE = "https://api.themoviedb.org/3/movie";
+
+// TMDB image base URL
+export const IMG_URL = "https://image.tmdb.org/t/p/original";
+export const API_URL_GENRES = "https://api.themoviedb.org/3/genre/movie/list";
+
+//https://api.themoviedb.org/3/discover/movie?with_genres=28&page=1
+
+
+export const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmQyOTEyNTYyODIyNDI0NjQ4MjQxZWQyOGEwZjc4NSIsIm5iZiI6MTYwMTkyNjU2MS4wMDgsInN1YiI6IjVmN2I3NWExZTAzOWYxMDAzN2YzNTliNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2yL4wX1lo5KmGhSJxzH2WK7DNtPhKjH1lDQ5PkIV_g";
+
+
+

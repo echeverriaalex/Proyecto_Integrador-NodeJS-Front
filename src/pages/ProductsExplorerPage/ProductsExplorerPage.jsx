b9d459385bff5cd94f0c40cf2d5f089ba85e3408
Alex Echeverria <aleex.naahuel@outlook.com>
2025-12-08 18:51:09 -0300

    correccion de errores y limpieza de codigo

--- src/pages/ProductsExplorerPage/ProductsExplorerPage.jsx
+++ src/pages/ProductsExplorerPage/ProductsExplorerPage.jsx
@@ -3,39 +3,35 @@ import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { isError, isFetching, success } from "../../redux/slice/productsSlice";
 import { isError as isErrorGenres, isFetching as isFetchingGenres, success as successGenres } from "../../redux/slice/genresSlice";
-import GenresSliderSkeleton from "../../components/GenreSlider/GenreSliderSkeleton";
-import Genres from "../../components/Genres/Genres";
+import { setTypeProductShow } from "../../redux/slice/typeProductShowSlice";
 import { GenresListContainerStyled, ProductsExplorerPageWrapper } from "./ProductsExplorerPageStyles";
 import { selectFetchGenreByType, selectFetchGenreProductsByTypePaginated } from "../../utils/extraFunctions";
 import GenresSlider from "../../components/GenreSlider/GenreSlider";
+import Genres from "../../components/Genres/Genres";
+import GenresSliderSkeleton from "../../components/GenreSlider/GenreSliderSkeleton";
 
 const ProductsExplorerPage = () => {
   
   const dispatch = useDispatch();
   const location = useLocation();
   const [showLoading, setShowLoading] = useState(true);
-  const [genresAndLists, setGenresAndLists] = useState([]);
-  //const { genresList, isLoading: isLoadingGenres, error: errorGenres} = useSelector((state) => state.genres);
   const { productList } = useSelector((state) => state.products);
-  const { typeProduct } = useSelector((state) => state.typeProductShow);
   const [categoryProduct, setCategoryProduct] = useState();
 
   useEffect(() => {
     const category = location.pathname === "/tvseries" ? "tvseries" : "movies";
-    //dispatch(success)
+    dispatch(setTypeProductShow(category));
     setCategoryProduct(category);
   }, [location.pathname]);
 
   const fetchGenresList = async () => {
-    //console.log("Category fetch genres list ", categoryProduct);
     try{
       dispatch(isFetchingGenres());
       const fetchGenresFunction = selectFetchGenreByType(categoryProduct);
       const genresListProduct = await fetchGenresFunction();
       
-      if(genresListProduct) {
-        dispatch(successGenres(genresListProduct.genres));        
-        //console.log(genresListProduct);
+      if(genresListProduct){
+        dispatch(successGenres(genresListProduct.genres));
         return genresListProduct;
       }
       else{
@@ -55,7 +51,6 @@ const ProductsExplorerPage = () => {
     try{
       dispatch(isFetching());
       const fetchFunction = selectFetchGenreProductsByTypePaginated(categoryProduct);
-
       const genresWithLists = await Promise.all(
         genresListProduct.genres.map(async (genre) => {
           const productData = await fetchFunction(genre.id, 1); // Always fetch page 1
@@ -65,14 +60,7 @@ const ProductsExplorerPage = () => {
           };
         })
       );
-
-      setGenresAndLists(genresWithLists);
       dispatch(success(genresWithLists));
-
-      console.log(genresWithLists);
-      
-
-
     } catch (error) {
       dispatch(isError(error));
       console.error(`Error loading Explorer Page.`, error);
@@ -85,7 +73,6 @@ const ProductsExplorerPage = () => {
     if (!categoryProduct) return;
 
     setShowLoading(true);
-    setGenresAndLists([]);
     fetchProductsAndGenres();
 
     const timer = setTimeout(() => {
@@ -105,7 +92,12 @@ const ProductsExplorerPage = () => {
           Array.from({ length: 1 }).map((_, index) => <GenresSliderSkeleton key={index} />)
         ) : (
           productList.map((genreItem, index) => (
-            <GenresSlider key={index} category={categoryProduct} genre={genreItem.genre} itemsList={genreItem.list} />
+            <GenresSlider 
+              key={index}
+              genre={genreItem.genre}
+              itemsList={genreItem.list}
+              category={categoryProduct}
+            />
           ))
         )}
       </GenresListContainerStyled>

f9f766c21842cd0d3467525e2f4fee18a9ff9d04
Alex Echeverria <aleex.naahuel@outlook.com>
2025-12-08 18:48:49 -0300

    nuevas constantes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,6 +8,8 @@ export const API_URL_TV = "https://api.themoviedb.org/3/tv";
 // URL movies pages API endpoint
 export const API_URL_MOVIES_PAGES = "https://api.themoviedb.org/3/discover/movie";
 export const API_URL_TV_PAGES = "https://api.themoviedb.org/3/discover/tv";
+// Example genre-specific API endpoints with action genre (id: 28) and page 1
+// https://api.themoviedb.org/3/discover/movie?with_genres=28&page=1
 
 // TMDB image base URL
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
@@ -17,13 +19,6 @@ export const API_URL_MOVIES_GENRES = "https://api.themoviedb.org/3/genre/movie/l
 export const API_URL_TV_GENRES = "https://api.themoviedb.org/3/genre/tv/list";
 
 
-
-
-
-//https://api.themoviedb.org/3/discover/movie?with_genres=28&page=1
-
-
 export const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmQyOTEyNTYyODIyNDI0NjQ4MjQxZWQyOGEwZjc4NSIsIm5iZiI6MTYwMTkyNjU2MS4wMDgsInN1YiI6IjVmN2I3NWExZTAzOWYxMDAzN2YzNTliNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F2yL4wX1lo5KmGhSJxzH2WK7DNtPhKjH1lDQ5PkIV_g";
-
 
 

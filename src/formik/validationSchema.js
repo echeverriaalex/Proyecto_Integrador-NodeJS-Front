56c2d4912835b84b3668f6f6ee6ed2f5161bf9b8
Alex Echeverria <aleex.naahuel@outlook.com>
2026-01-11 11:57:10 -0300

    modifico validaciones

--- src/formik/validationSchema.js
+++ src/formik/validationSchema.js
@@ -22,12 +22,16 @@ export const checkoutValidationSchema = Yup.object({
 
 export const registerValidationSchema = Yup.object({
     name: Yup.string()
-        .min(2, 'Too Short!')
-        .max(100, 'Too Long!')
+        .min(2, 'Too Short! Min 2 characters')
+        .max(40, 'Too Long! Max 40 characters')
         .required('Required'),
     email: Yup.string().email('Invalid email').required('Required'),
     password: Yup.string()
         .min(8, 'Password must be at least 8 characters')
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9\s]).{8,}$/,
+            'Password must include uppercase, lowercase, number, and special character'
+        )
         .required('Required'),
 });
 

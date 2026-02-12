bb5e71aba85a379dba5c0850802d04e352006d0f
Alex Echeverria <aleex.naahuel@outlook.com>
2026-02-11 22:02:37 -0300

    reestructuro

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,88 @@
-import { ExtraContainerStyled, FooterContainerStyled, LinkContainerStyled, LinksContainerStyled, MenuContainerStyled, SectionsContainerStyled } from "./FooterStyles";
-import GenresSection from "./GenresSection/GenresSection";
+import { useNavigate } from "react-router-dom";
+import { ExtraContainerStyled, FooterContainerStyled, GenresFooterContainerStyled, ItemFooterContainerStyled, ItemsFooterContainerStyled, LinkContainerStyled, LinksContainerStyled, MenuContainerStyled, SectionGenresContainerStyled, SectionsFooterContainerStyled } from "./FooterStyles";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { getMoviesGenres } from "../../axios/axios-movies";
+import { getTvSeriesGenres } from "../../axios/axios-tvseries";
 
 const Footer = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+
+    const [moviesGenresList, setMoviesGenresList] = useState([]);
+    const [tvseriesGenresList, setTvseriesGenresList] = useState([]);
+
+    //const { genresList, isLoading, error} = useSelector((state) => state.genres);
+
+    const fetchGenresList = async () => {
+        try{
+            //dispatch(isFetching());
+            const moviesGenres = await getMoviesGenres();
+            console.log("MOVIES", moviesGenres)
+            setMoviesGenresList(moviesGenres?.genres || []);
+
+            const tvseriesGenres = await getTvSeriesGenres();
+            setTvseriesGenresList(tvseriesGenres?.genres || []);
+
+            /*
+            if(genresList) {
+                dispatch(success(genresList?.genres));
+            }
+            else{
+                console.error(`Genre ${genre} not found.`);
+            }
+            */
+        }catch(error){
+            //dispatch(isError(error));
+            console.error(`Error loading genres on footer.`, error);
+        }
+    }
+
+    useEffect(() => {
+        fetchGenresList();
+
+        //console.log("MOVIES", moviesGenresList)
+            
+            
+        console.log("TV SERIES", tvseriesGenresList);
+        
+    }, []);
+    
     return (
         <FooterContainerStyled>
-            <SectionsContainerStyled>
-                <GenresSection />
+            <SectionsFooterContainerStyled>
+                <SectionGenresContainerStyled>
+                    <GenresFooterContainerStyled>
+                        <h2>Movies</h2>
+                        <ItemsFooterContainerStyled>
+                            {moviesGenresList.map((genre) => (
+                                <ItemFooterContainerStyled
+                                    key={genre?.id}
+                                    //onClick={() => navigate(`/genres/${genre.id}`)}
+                                    onClick={() => navigate(`/movies/${genre.name}`)}
+                                >
+                                    <p className="font-bold"> {genre?.name} </p>
+                                </ItemFooterContainerStyled>
+                            ))}
+                        </ItemsFooterContainerStyled>
+                    </GenresFooterContainerStyled>
+                    <GenresFooterContainerStyled>
+                        <h2>Series</h2>
+                        <ItemsFooterContainerStyled>
+                            {tvseriesGenresList.map((genre) => (
+                                <ItemFooterContainerStyled 
+                                    key={genre?.id}
+                                    //onClick={() => navigate(`/genres/${genre.id}`)}
+                                    onClick={() => navigate(`/tvseries/${genre.name}`)}
+                                >
+                                    <p className="font-bold"> {genre?.name} </p>
+                                </ItemFooterContainerStyled>
+                            ))}
+                        </ItemsFooterContainerStyled>
+                    </GenresFooterContainerStyled>
+                </SectionGenresContainerStyled>
 
                 <MenuContainerStyled>
                     <h2 className="text-xl font-bold text-white">Menu</h2>
@@ -19,9 +96,7 @@ const Footer = () => {
                         <LinkContainerStyled to="/privacy">Privacy Policy</LinkContainerStyled>
                     </LinksContainerStyled>
                 </MenuContainerStyled>
-
-
-            </SectionsContainerStyled>
+            </SectionsFooterContainerStyled>
             <ExtraContainerStyled className="container mx-auto text-center">
                 <p className="font-bold text-left text-white">&copy; {new Date().getFullYear()} Developed by Alex Nahuel Echeverria.</p> 
                 <p className="font-bold text-left text-white">Made with ❤️</p>
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,17 +1,18 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { themePage } from "../styles/GlobalStyles";
 
 export const FooterContainerStyled = styled.div`
     display: flex;
-    //flex-direction: column;
     align-items: center;
     justify-content: center;
     flex-wrap: wrap;
-    //background-color: #2d3748;
-    background-color: #041b41ff;
     padding: 50px;
     gap: 40px;
     width: 100%;
+    background-color: ${themePage.colors.bgGray};
+
+    //background: #303030;
 
     @media (max-width: 576px){
         padding: 50px 5px;
@@ -19,19 +20,102 @@ export const FooterContainerStyled = styled.div`
     }
 `;
 
-export const SectionsContainerStyled = styled.div`
+export const SectionsFooterContainerStyled = styled.div`
     display: flex;    
     justify-content: center;
     align-items: center;
     padding: 10px;
     gap: 20px;
     width: 100%;
-    //background-color: #0d8373ff;
+    
+    //background-color: #327223;
 
     @media (max-width: 576px){
         padding: 5px;
         flex-direction: column;
-        //background-color: #e02bc8ff;
+    }
+`;
+
+export const SectionGenresContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    padding: 10px;
+    width: 100%;
+
+    //background: #5f5f5f;
+`;
+
+export const GenresFooterContainerStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+    align-items: center;
+    padding: 50px 30px;
+    width: 100%;
+    border-radius: 10px;
+
+    background: #1b1b1b;
+
+    h2 {
+        width: 100%;
+        text-align: left;
+        //background-color: #1815b3;
+        font-size: 1.5rem;
+        font-weight: bold;
+    }
+`;
+
+export const ItemsFooterContainerStyled = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+    //background-color: #1eafa3ff;
+    transition: all .3s linear;
+    width: 100%;
+    //padding: 10px;
+    
+    p {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        color: white;
+        background-color: #000;
+        border-radius: 8px;
+    }
+`;
+
+export const ItemFooterContainerStyled = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;    
+    cursor: pointer;
+    //background-color: #2e1adb;
+    transition: all .3s linear;
+    width: 100%;
+    height: 50px;
+    
+    &:hover {
+        //background-color: #148888ff;
+        border: 3px dotted #fff;
+    }
+
+    p {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        color: white;
+        background-color: #000;
+        border-radius: 8px;
     }
 `;
 

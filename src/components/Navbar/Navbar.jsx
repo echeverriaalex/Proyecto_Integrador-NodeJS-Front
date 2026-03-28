2f8df9e4e0ba27041a20363667b6872e5d21954a
Alex Echeverria <aleex.naahuel@outlook.com>
2026-03-27 21:23:48 -0300

    limpieza de codigo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,6 @@
 import SearchBar from "../UI/SearchBar/SearchBar";
 import { BlockIconsContainerStyled, ElementsContainerStyled, IconsContainerStyled, LinkContainerStyled, LinksContainerStyled, LogoContainerStyled, MainNavbarWrapper, MobileContainerStyled, NavbarWrapper, UserContainerSessionStyled, UserContainerStyled } from "./NavbarStyles";
-//import Logo from "../../assets/cinespace-logo.png";
-//import Logo from "../../assets/infinity-watch.png";
-//import Logo from "../../assets/infinity-watch2.png";
-//import Logo from "../../assets/infinity-watch3.png";
 import Logo from "../../assets/Infinity-Symbol.svg";
-
-//import { User } from 'lucide-react';
-
-
 import ModalMenu from "./Modals/ModalMenu";
 import MenuIcon from "./components/MenuIcon/MenuIcon";
 import CartIcon from "./components/CartIcon/CartIcon";
@@ -16,7 +8,6 @@ import ModalCart from "./Modals/ModalCart";
 import Button from "../UI/Button/Button";
 import ButtonLink from "../UI/ButtonLink/ButtonLink";
 import { useDispatch, useSelector } from "react-redux";
-
 import { setCurrentUser } from "../../redux/users/userSlice";
 import { useNavigate } from "react-router-dom";
 import { formatUserName } from "../../utils/extraFunctions";
@@ -27,10 +18,6 @@ const Navbar = () => {
     const { currentUser } = useSelector(state => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
-
-    //console.log("current user: ", currentUser?.name);
-    //console.log("All user data: ", currentUser);
 
     return (
         <NavbarWrapper className="bg-blue-200 p-4">
@@ -60,21 +47,11 @@ const Navbar = () => {
                                 : <ButtonLink route="/login" $bgColor="#464646ff">Login</ButtonLink>
                         }
                         <CartIcon />
-                        {
-                            /*
-                            currentUser ?
-                                <UserContainerSessionStyled>
-                                    <p>{ formatUserName(currentUser?.name) }</p>
-                                </UserContainerSessionStyled> 
-                                : <LinkContainerStyled to="/login">LogIn</LinkContainerStyled>
-                                */
-                        }
                     </BlockIconsContainerStyled>
                 </IconsContainerStyled>
                 <SearchBar />
             </MobileContainerStyled>
-
-
+            
             <MainNavbarWrapper>
                 <LogoContainerStyled to="/">
                     <img src={Logo} alt="Logo"/>
@@ -84,43 +61,15 @@ const Navbar = () => {
                     <LinkContainerStyled to="/">Home</LinkContainerStyled>
                     <LinkContainerStyled to="/tvseries">Tv_&_Series</LinkContainerStyled>
                     <LinkContainerStyled to="/movies">Movies</LinkContainerStyled>
-                    {
-                        /*
-                        currentUser && <LinkContainerStyled to="/mylist">My_List</LinkContainerStyled>
-                        */
-                    }
                     <LinkContainerStyled to="/about">About</LinkContainerStyled>
                     <LinkContainerStyled to="/contact">Contact</LinkContainerStyled>
-                    <LinkContainerStyled to="/mypurchases">My_Purchases</LinkContainerStyled>
-                    {
-                        /*
-                        currentUser ?
-                            <Button onClick={()=>{
-                                dispatch(setCurrentUser(null));
-                                    navigate("/");
-                                }}
-                            >
-                                Logout
-                            </Button>
-                            :
-                            <ButtonLink route="/login" $bgColor="#464646ff">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-user-icon lucide-user">
-                                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/>
-                                    <circle cx="12" cy="7" r="4"/>
-                                </svg>
-                                Login
-                            </ButtonLink>
-                        */
-                    }
+                    {currentUser && (
+                        <LinkContainerStyled to="/mypurchases">My_Purchases</LinkContainerStyled>
+                    )}
                 </LinksContainerStyled>
                 <ElementsContainerStyled>
                     <SearchBar />
                     <CartIcon />
-                    {
-                    /*
-                        <MenuIcon />
-                    */
-                    }
                     {
                         currentUser ?
                             <UserContainerSessionStyled>
@@ -146,7 +95,6 @@ const Navbar = () => {
                     }
                 </ElementsContainerStyled>
             </MainNavbarWrapper>
-            
         </NavbarWrapper>
     )
 }
